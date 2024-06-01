cf4e37ba47d580ff8ec78220296041df78e8a4bd
Nilesh Dubey <dubeynick123@gmail.com>
2024-06-01 15:33:04 +0530

    add send sms permission

--- Pages/DeviceBinding/index.tsx
+++ Pages/DeviceBinding/index.tsx
@@ -2,10 +2,26 @@ import React, { useEffect } from "react";
 import { View, Text, Alert, Button } from "react-native";
 import * as LocalAuthentication from "expo-local-authentication";
 import * as SMS from "expo-sms";
+
 const DeviceBinding = () => {
+  const isAvailable = async () => {
+    await SMS.isAvailableAsync()
+      .then((permission) => {
+        console.log(permission === true);
+        if (permission) {
+          alert("permission done");
+          SMS.sendSMSAsync(["8657828994"], "My sample HelloWorld message");
+        } else if (!permission) {
+          alert("sms not ");
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
   return (
     <View>
-      <Text>heelo</Text>
+      <Button title="permission" onPress={() => isAvailable()} />
     </View>
   );
 };
